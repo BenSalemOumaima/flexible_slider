47f7352929b89d2631d3a4d850f9e5469ebd11c0
OUMAIMA BS <bnsalemoumaima@gmail.com>
2022-05-21 22:51:42 +0100

    first commit

---
+++ lib/flexible_slider.dart
@@ -0,0 +1,12 @@
+library flexible_slider;
+
+import 'package:flutter/material.dart';
+
+class FlexibleSlider extends StatelessWidget {
+  const FlexibleSlider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
